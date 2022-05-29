fc9faafa189d4d4c5434556c2f05b2694b7e2e80
IvanovskiStefan <96203470+IvanovskiStefan@users.noreply.github.com>
2022-05-29 22:32:02 +0200

    Pussh domasna class 2:D

---
+++ Homeworks/Class02/HW_2_Query1.sq.sql
@@ -0,0 +1,37 @@
+use SEDC ;
+
+select * from Employees 
+where FirstName = 'Goran'
+
+
+
+select * from Employees
+
+WHERE LastName LIKE  'S%'
+
+
+
+select * from Employees
+
+WHERE DateOfBirth >  '1988.01.01'
+
+
+
+select * from Employees
+
+WHERE Gender = 'm'
+
+
+
+select * from Employees
+
+WHERE Gender = 'm'
+
+select * from Employees
+
+WHERE HireDate = '1998/01/01'
+
+select * from Employees
+
+WHERE LastName LIKE  'A%' AND  HireDate = '1998/01/01'
+
